d09096ac40503e221e8e238d64cbc8f50d326b48
Devilharsha <95213910+Devilharsha@users.noreply.github.com>
2022-01-12 22:31:59 +0530

    Update Repo.py

--- MashaRoBot/modules/Repo.py
+++ MashaRoBot/modules/Repo.py
@@ -6,20 +6,20 @@ from MashaRoBot.conf import get_str_key
 from MashaRoBot import pbot
  
  # pls don't delete
-REPO_TEXT = "**Thanimai [BOT](https://telegra.ph/file/526ed899597d7827474a1.jpg) will Make Your Groups Secured And it's have a lot of fun features (:  ! \n\n↼ Owner ⇀ : 『 [Telegram pro](t.me/TheTelegrampro) 』\n╭──────────────\n┣─ » Python ~ 3.8.6\n┣─ » Update ~ Recently\n╰──────────────\n\n»»» @THANIMAIBOTS «««"
+REPO_TEXT = "**seto Kaiba [BOT](https://telegra.ph/file/bcb8b7df34569d73ef021.jpg) will Make Your Groups Secured And it's have a lot of fun features (:  ! \n\n↼ Owner ⇀ : 『 [Telegram pro](t.me/TheTelegrampro) 』\n╭──────────────\n┣─ » Python ~ 3.8.6\n┣─ » Update ~ Recently\n╰──────────────\n\n»»» @THANIMAIBOTS «««"
   
 BUTTONS = InlineKeyboardMarkup(
       [[
-        InlineKeyboardButton("ʀᴇᴘᴏꜱɪᴛᴏʀʏ", url=f"https://github.com/proTamizhan/thanimaibot"),
-        InlineKeyboardButton("Gɪᴛʜᴜʙ", url=f"https://github.com/proTamizhan"),
+        InlineKeyboardButton("ʀᴇᴘᴏꜱɪᴛᴏʀʏ", url=f"https://t.me/PegasusUpdates"),
+        InlineKeyboardButton("Gɪᴛʜᴜʙ", url=f"https://github.com/devilharsha"),
       ],[
-        InlineKeyboardButton("ᴏᴡɴᴇʀ ❣️", url="https://t.me/TheTelegrampro"),
-        InlineKeyboardButton("ꜱᴜᴘᴘᴏʀᴛ", url="https://t.me/thanimaisupport"),
-       InlineKeyboardButton("ᴅᴇᴠᴇʟᴏᴘᴇʀ", url="https://t.me/Kittu_the_criminall"),
+        InlineKeyboardButton("ᴏᴡɴᴇʀ ❣️", url="https://t.me/harshahero"),
+        InlineKeyboardButton("ꜱᴜᴘᴘᴏʀᴛ", url="https://t.me/Pegasussupportchat"),
+       InlineKeyboardButton("ᴅᴇᴠᴇʟᴏᴘᴇʀ", url="https://t.me/Tusukasa"),
       ],[
-        InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇꜱ", url="https://t.me/THANIMAIBOTS"),
-        InlineKeyboardButton("ᴅᴇᴠᴇʟᴏᴘᴇʀ", url="https://t.me/ctzfamily"),
-       InlineKeyboardButton("ᴅᴇᴠᴇʟᴏᴘᴇʀ", url="https://t.me/Sarbudeen786"),
+        InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇꜱ", url="https;//t.me/PegasusUpdates"),
+        InlineKeyboardButton("ᴅᴇᴠᴇʟᴏᴘᴇʀ", url="https://t.me/harshahero"),
+       InlineKeyboardButton("ᴅᴇᴠᴇʟᴏᴘᴇʀ", url="https://t.me/Pegasussupportchat"),
       ]]
     )
   
