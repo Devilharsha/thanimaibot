f7184ee4d9398b83b6c3b9e602caa134da19699e
Harsha <95213910+Devilharsha@users.noreply.github.com>
2022-01-29 07:09:28 +0530

    Update welcome_sql.py

--- MashaRoBot/modules/sql/welcome_sql.py
+++ MashaRoBot/modules/sql/welcome_sql.py
@@ -4,7 +4,7 @@ from typing import Union
 
 from MashaRoBot.modules.helper_funcs.msg_types import Types
 from MashaRoBot.modules.sql import BASE, SESSION
-from sqlalchemy import BigInteger, Boolean, Column, Integer, String, UnicodeText
+from sqlalchemy import BigInteger, Boolean, Column, BigInteger, String, UnicodeText
 
 DEFAULT_WELCOME = 'ஏய் {first}, நீங்கள் எப்படி இருக்கிறீர்கள்?'
 DEFAULT_GOODBYE = 'பேச தெரிஞ்சா பேசு தேவையில்லாம பேசி அடிவாங்கி சாகதா 🤬!'
@@ -171,10 +171,10 @@ class Welcome(BASE):
     custom_welcome = Column(
         UnicodeText, default=random.choice(DEFAULT_WELCOME_MESSAGES)
     )
-    welcome_type = Column(Integer, default=Types.TEXT.value)
+    welcome_type = Column(BigInteger, default=Types.TEXT.value)
 
     custom_leave = Column(UnicodeText, default=random.choice(DEFAULT_GOODBYE_MESSAGES))
-    leave_type = Column(Integer, default=Types.TEXT.value)
+    leave_type = Column(BigInteger, default=Types.TEXT.value)
 
     clean_welcome = Column(BigInteger)
 
@@ -231,7 +231,7 @@ class WelcomeMute(BASE):
 
 class WelcomeMuteUsers(BASE):
     __tablename__ = "human_checks"
-    user_id = Column(Integer, primary_key=True)
+    user_id = Column(BigInteger, primary_key=True)
     chat_id = Column(String(14), primary_key=True)
     human_check = Column(Boolean)
 
